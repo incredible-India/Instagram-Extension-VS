36c09c620521a41be4b53028e498ac9a2ae3b38c
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-12-25 15:56:37 +0530

    added instagram

---
+++ Instagram/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Instagram
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "Instagram";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("2bcc590f-ecb3-4ae0-9d1b-4ba05e4819fe")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
